b22ec2d1fa9f136c44d09df81e3113e25b2f9f0a
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2023-09-03 23:03:32 +0200

    Add initial structure

--- 2015/Rust/src/main.rs
+++ 2015/Rust/src/main.rs
@@ -1,3 +1,12 @@
+mod days;
+
 fn main() {
     println!("Hello, world!");
+
+    // It's ok to copy a string literal like this, since the amount of memory required is known at
+    // compile time.
+    let x = "Hello";
+    let y = x;
+
+    println!("x = {}, y = {}", x, y)
 }
