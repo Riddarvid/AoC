86c59f229b4ef3796428b670a35626dc887e8b3b
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2023-12-05 11:36:04 +0100

    Update utils version

--- src/2023/Haskell/app/Main.hs
+++ src/2023/Haskell/app/Main.hs
@@ -1,9 +1,9 @@
 module Main (main) where
 import           AoCUtils.Interactive (aocMain)
+import AoCUtils.Config (mkConfig)
 import           Utils.Days           (solvers)
 
 main :: IO ()
-main = aocMain solvers visualizations
-
-visualizations :: String -> IO ()
-visualizations visId = error $ "No visualization implemented for " ++ visId
+main = aocMain cfg
+  where
+    cfg = mkConfig solvers "../../../data/inputs/2023" "../../../data/results/2023" Nothing
