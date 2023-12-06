55cade317b7c38175f3921623e346ae2484b1689
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2023-12-06 09:16:49 +0100

    2023 Day6 Haskell

---
+++ src/2023/Haskell/src/Days/Day6.hs
@@ -0,0 +1,43 @@
+module Days.Day6 (solve) where
+import           AoCUtils.Days  (Solver)
+import           AoCUtils.Regex (parseUnsignedInts)
+import           Debug.Trace    (traceShow, traceShowId)
+
+solve :: Solver
+solve input = let
+  races = parseRaces input
+  part1 = solutionProducts races
+  race = parseRace input
+  part2 = solutionProducts [race]
+  in (show part1, show part2)
+
+data Race = Race Integer Integer
+
+parseRaces :: String -> [Race]
+parseRaces input = zipWith Race times distances
+  where
+    lines' = lines input
+    times = parseUnsignedInts $ head lines'
+    distances = parseUnsignedInts $ lines' !! 1
+
+parseRace :: String -> Race
+parseRace input = Race (head times) (head distances)
+  where
+    lines' = lines input
+    times = parseUnsignedInts $ filter (/= ' ') $ head lines'
+    distances = parseUnsignedInts $ filter (/= ' ') $ lines' !! 1
+
+solutionProducts :: [Race] -> Integer
+solutionProducts = product . traceShowId . map nSolutions
+
+nSolutions :: Race -> Integer
+nSolutions (Race t d) = traceShow (mid, diff, comp) $ floor (mid + diff) - ceiling (mid - diff) + 1 + comp
+  where
+    mid = fromIntegral t / 2
+    diff = sqrt (fromIntegral (t ^ 2 - 4 * d)) / 2
+    comp = if isInt $ mid + diff
+      then (-2)
+      else 0
+
+isInt :: ( RealFrac a) =>a -> Bool
+isInt x = floor x == ceiling x
--- src/2023/Haskell/src/Utils/Days.hs
+++ src/2023/Haskell/src/Utils/Days.hs
@@ -6,6 +6,7 @@ import           Days.Day2     as Day2 (solve)
 import           Days.Day3     as Day3 (solve)
 import           Days.Day4     as Day4 (solve)
 import           Days.Day5     as Day5 (solve)
+import           Days.Day6     as Day6 (solve)
 
 solvers :: [Solver]
-solvers = [Day1.solve, Day2.solve, Day3.solve, Day4.solve, Day5.solve]
+solvers = [Day1.solve, Day2.solve, Day3.solve, Day4.solve, Day5.solve, Day6.solve]
