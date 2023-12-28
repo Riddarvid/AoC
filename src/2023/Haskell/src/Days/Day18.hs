571e7373617b4e3a9185fcde61590896f01cc814
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2023-12-28 02:19:51 +0100

    2023 Day 18 Haskell

--- src/2023/Haskell/src/Days/Day18.hs
+++ src/2023/Haskell/src/Days/Day18.hs
@@ -2,35 +2,37 @@ module Days.Day18 (solve) where
 import           AoCUtils.Days      (Solver)
 import           AoCUtils.Geometry  (Point (moveBy, scaleBy),
                                      Point2 (P2, p2X, p2Y), Vector2, downV,
-                                     findDimensions, leftV, rightV, upV)
-import           AoCUtils.Graphs    (BfsState (bfsPreMap), Goal (GFull),
-                                     bfsExplore)
-import           AoCUtils.Show      (showPoints)
+                                     leftV, rightV, upV)
 import           Data.Foldable      (foldl')
 import           Data.Function      (on)
-import qualified Data.HashMap.Lazy  as HM
-import           Data.HashSet       (HashSet)
-import qualified Data.HashSet       as HS
-import           Data.List          (elemIndex, intersect, nub, sort, sortOn,
-                                     (\\))
+import           Data.List          (sort, sortOn, (\\))
 import           Data.List.NonEmpty (NonEmpty ((:|)), (<|))
 import qualified Data.List.NonEmpty as NE
-import           Data.Maybe         (fromJust)
-import           Debug.Trace        (trace, traceShow, traceShowId)
+import           Debug.Trace        (traceShow)
 
 type Pos = Point2 Int
 
 data Dir = DUp | DLeft | DDown | DRight
   deriving (Show)
 
+dirV :: Dir -> Vector2 Int
+dirV DUp    = upV
+dirV DLeft  = leftV
+dirV DDown  = downV
+dirV DRight = rightV
+
 data Instruction = Instr Dir Int String
   deriving (Show)
+
+instrToVec :: Instruction -> Vector2 Int
+instrToVec (Instr dir mag _) = dirV dir `scaleBy` mag
 
 solve :: Solver
 solve input = let
   instrs = NE.map parseInstr $ NE.fromList $ lines input
   part1 = solve1 instrs
-  in (show part1, "")
+  part2 = solve2 instrs
+  in (show part1, show part2)
 
 parseInstr :: String -> Instruction
 parseInstr input = Instr dir (read l) (drop 1 color')
@@ -46,21 +48,24 @@ parseInstr input = Instr dir (read l) (drop 1 color')
 solve1 :: NonEmpty Instruction -> Integer
 solve1 = solveGeneral
 
-digEdge :: [Instruction] -> HashSet Pos
-digEdge = snd . foldl' (uncurry digInstr) (P2 0 0, HS.empty)
+solve2 :: NonEmpty Instruction -> Integer
+solve2 = solveGeneral . NE.map flipInstruction
 
-digInstr :: Pos -> HashSet Pos -> Instruction -> (Pos, HashSet Pos)
-digInstr p edge (Instr dir length' _) = (p', HS.union edge $ HS.fromList edge')
+flipInstruction :: Instruction -> Instruction
+flipInstruction (Instr _ _ color) = Instr (parseDir dirChar) (parseDist distStr) color
   where
-    dirV' = dirV dir
-    edge' = [p `moveBy` (dirV' `scaleBy` s) | s <- [1 .. length']]
-    p' = p `moveBy` (dirV' `scaleBy` length')
+    distStr = take 5 $ drop 2 color
+    dirChar = color !! 7
 
-dirV :: Dir -> Vector2 Int
-dirV DUp    = upV
-dirV DLeft  = leftV
-dirV DDown  = downV
-dirV DRight = rightV
+parseDist :: String -> Int
+parseDist str = read ("0x" ++ str)
+
+parseDir :: Char -> Dir
+parseDir '0' = DRight
+parseDir '1' = DDown
+parseDir '2' = DLeft
+parseDir '3' = DUp
+parseDir _   = DRight
 
 solveGeneral :: NonEmpty Instruction -> Integer
 solveGeneral = calcArea . findCorners
@@ -71,12 +76,10 @@ findCorners = NE.fromList
   . foldl' (\acc@(p :| _) v -> (p `moveBy` v) <| acc) (P2 0 0 :| [])
   . NE.map instrToVec
 
-instrToVec :: Instruction -> Vector2 Int
-instrToVec (Instr dir mag _) = dirV dir `scaleBy` mag
-
 formYGroups :: NonEmpty Pos -> NonEmpty (Int, NonEmpty Int)
-formYGroups =
-  NE.map (\ps -> (p2X $ NE.head ps, NE.map p2Y ps)) . NE.groupBy1 ((==) `on` p2X) . NE.sortWith p2X
+formYGroups = NE.map (\ps -> (p2X $ NE.head ps, NE.map p2Y ps))
+  . NE.groupBy1 ((==) `on` p2X)
+  . NE.sortWith p2X
 
 findDistances :: NonEmpty (Int, NonEmpty Int) -> [Int]
 findDistances (_ :| [])     = []
@@ -101,7 +104,6 @@ mergeRanges :: [(Int, Int)] -> [Int]
 mergeRanges []                     = []
 mergeRanges [(a, b)]               = [a, b]
 mergeRanges ((a, b) : (c, d) : xs)
-
   | b >= c = mergeRanges ((min a c, max b d) : xs)
   | otherwise = a : b : mergeRanges ((c, d) : xs)
 
