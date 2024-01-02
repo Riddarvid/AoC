c053d543c8b162df2f6227ddb57cf3948ce1e5a9
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2024-01-02 12:58:29 +0100

    2023 Day 21 Haskell part 2

--- src/2023/Haskell/app/Main.hs
+++ src/2023/Haskell/app/Main.hs
@@ -5,6 +5,7 @@ import           AoCUtils.Interactive (aocMain)
 import           Days                 (solvers)
 import           Days.Day10.Graphics  (renderDay10Loop, renderDay10LoopScaled)
 import           Days.Day14.Graphics  (animateDay14)
+import           Days.Day21.Graphics  (displayDay21)
 
 inputPath :: FilePath
 inputPath = "../../../data/inputs/2023"
@@ -18,6 +19,7 @@ visualizations :: String -> IO ()
 visualizations "10l"  = vis10Loop
 visualizations "10ls" = vis10LoopScaled
 visualizations "14"   = vis14
+visualizations "21"   = vis21
 visualizations _      = error "No matching visualization"
 
 vis10Loop :: IO ()
@@ -34,3 +36,8 @@ vis14 :: IO ()
 vis14 = do
   input <- readInput inputPath 14
   animateDay14 input
+
+vis21 :: IO ()
+vis21 = do
+  input <- readInput inputPath 21
+  displayDay21 input
--- src/2023/Haskell/src/Days/Day21.hs
+++ src/2023/Haskell/src/Days/Day21.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE TupleSections #-}
-module Days.Day21 (solve) where
+module Days.Day21 (solve, Tile(..), solveGraphics) where
 import           AoCUtils.Days     (Solver)
-import           AoCUtils.Geometry (Point2 (P2), downV, leftV, moveBy, rightV,
-                                    upV)
+import           AoCUtils.Geometry (Point (scaleBy), Point2 (P2), downV, leftV,
+                                    moveBy, rightV, upV)
 import           AoCUtils.Matrices (matrixToHashMap)
 import           Data.HashMap.Lazy (HashMap)
 import qualified Data.HashMap.Lazy as HM
+import           Data.HashSet      (HashSet)
+import qualified Data.HashSet      as HS
+import           Data.Maybe        (fromJust)
 import           Utils.Dijkstra    (exploreFully)
 
 type Pos = Point2 Int
@@ -13,16 +16,20 @@ type Pos = Point2 Int
 data Tile = Plot | Rock
   deriving (Eq)
 
+data Parity = Even | Odd
+  deriving (Eq)
+
 solve :: Solver
 solve input = let
-  (start, tiles) = parseInput input
+  (maxX, maxY, start, tiles) = parseInput input
   part1 = solve1 start tiles
-  in (show part1, "")
+  part2 = solve2 maxX maxY start tiles
+  in (show part1, show part2)
 
-parseInput :: String -> (Pos, HashMap Pos Tile)
-parseInput input = (start, HM.map parseTile chars)
+parseInput :: String -> (Int, Int, Pos, HashMap Pos Tile)
+parseInput input = (maxX, maxY, start, HM.map parseTile chars)
   where
-    (chars, _, _) = matrixToHashMap $ lines input
+    (chars, maxX, maxY) = matrixToHashMap $ lines input
     start = head $ HM.keys $ HM.filter (== 'S') chars
 
 parseTile :: Char -> Tile
@@ -32,12 +39,24 @@ parseTile '#' = Rock
 parseTile _   = error "Invalid tile"
 
 solve1 :: Pos -> HashMap Pos Tile -> Int
-solve1 start@(P2 startX startY) tiles = HM.size exactReachable
+solve1 = nExactReachableSearch Even (64 :: Int)
+
+nExactReachableSearch :: Integral a => Parity -> a -> Pos -> HashMap Pos Tile -> Int
+nExactReachableSearch parity steps start = HS.size . findExactReachableSearch parity steps start
+
+findExactReachableSearch :: Integral a => Parity -> a -> Pos -> HashMap Pos Tile -> HashSet Pos
+findExactReachableSearch parity steps start tiles = exactReachable
   where
     dists = exploreFully [start] (mkAdjacency tiles)
-    reachable = HM.filter (<= 64) dists
-    parity = (startX + startY) `mod` 2
-    exactReachable = HM.filterWithKey (\(P2 x y) _ -> (x + y) `mod` 2 == parity) reachable
+    reachable = HM.keysSet $ HM.filter (\d -> toInteger d <= toInteger steps) dists
+    exactReachable = HS.filter (isExactReachable parity start) reachable
+
+isExactReachable :: Parity -> Pos -> Pos -> Bool
+isExactReachable parity (P2 startX startY) (P2 x y) = (x + y) `mod` 2 == parityBit
+  where
+    parityBit = if parity == Odd
+      then (startX + startY + 1) `mod` 2
+      else (startX + startY) `mod` 2
 
 mkAdjacency :: HashMap Pos Tile -> Pos -> [(Pos, Int)]
 mkAdjacency tiles p = map (, 1) plotNeighbors
@@ -49,5 +68,58 @@ mkAdjacency tiles p = map (, 1) plotNeighbors
       Nothing   -> False
       Just tile -> tile == Plot
 
+solveGraphics :: String -> (Int, Int, HashMap Pos Tile, HashSet Pos)
+solveGraphics input = (maxX, maxY, tiles, exactReachable)
+  where
+    (maxX, maxY, start, tiles) = scaleMap 1 input
+    exactReachable = findExactReachableSearch Odd (65 :: Int) start tiles
+
+-- Factor must be odd for start position scaling to work
+scaleMap :: Int -> String -> (Int, Int, Pos, HashMap Pos Tile)
+scaleMap factor input = (maxX, maxY, startScaled, HM.map parseTile scaledChars)
+  where
+    baseGrid = lines input
+    row = map (concat . replicate factor) baseGrid
+    scaledGrid = concat $ replicate factor row
+    (scaledChars, maxX, maxY) = matrixToHashMap scaledGrid
+    (chars, _, _) = matrixToHashMap $ lines input
+    start = head $ HM.keys $ HM.filter (== 'S') chars
+    startScaled = start `moveBy`
+      (rightV `scaleBy` (length (head (lines input)) * (factor `div` 2)))
+      `moveBy`
+      (downV `scaleBy` (length (lines input) * (factor `div` 2)))
+
+
 -- Part 2
--- We can assume that in a lot of these maps, we will be able to reach every square within them
+
+solve2 :: Int -> Int -> Pos -> HashMap Pos Tile -> Integer
+solve2 maxX maxY start = fromJust . solveGeneral maxX maxY 26501365 start
+
+-- Checks that the input is a square, that the starting position is in the middle of it,
+-- and that the steps value can be expressed as n * 2 * side + mid
+solveGeneral :: Int -> Int -> Integer -> Pos -> HashMap Pos Tile -> Maybe Integer
+solveGeneral maxX maxY steps start tiles
+  | maxX /= maxY = Nothing
+  | even side = Nothing
+  | start /= P2 mid mid = Nothing
+  | (steps - toInteger mid) `mod` (2 * toInteger side) /= 0 = Nothing
+  | otherwise = Just $ solveGeneral' side steps tiles
+  where
+    side = maxX + 1 -- 0 indexing
+    mid = (side - 1) `div` 2
+
+solveGeneral' :: Int -> Integer -> HashMap Pos Tile -> Integer
+solveGeneral' side steps tiles = a * na + a' * na' + bb' * nbb'
+  where
+    mid = (side - 1) `div` 2
+    start = P2 mid mid
+    a = toInteger $ nExactReachableSearch Odd mid start tiles
+    a' = toInteger $ nExactReachableSearch Even (mid - 1) start tiles
+    aa'bb' = toInteger $ nExactReachableSearch Odd steps start tiles +
+      nExactReachableSearch Even steps start tiles
+    bb' = aa'bb' - a - a'
+
+    n = (steps - toInteger mid) `div` (2 * toInteger side)
+    na = (2 * n + 1) ^ (2 :: Integer)
+    na' = (2 * n) ^ (2 :: Integer)
+    nbb' = ((4 * n + 1) ^ (2 :: Integer) - na - na') `div` 2
